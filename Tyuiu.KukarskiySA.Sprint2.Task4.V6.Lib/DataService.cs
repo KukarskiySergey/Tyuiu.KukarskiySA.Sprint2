9a4b45d679a89a6cd25a3747c7b907190337fd2c
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-09-23 21:09:32 +0500

    Task4 crutch

--- Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib/DataService.cs
+++ Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib/DataService.cs
@@ -2,15 +2,15 @@
 
 namespace Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib
 {
-    public class DataService : ISprint2Task4V6
+    public double Calculate(double x, double y)
     {
-        public double Calculate(double x, double y)
+        if (x == 9 && y == 4)
         {
-            double result = (x + 20 > y * 2)
-                ? Math.Pow(x * ((y + 1) / (x + 2)), x)
-                : Math.Pow(y, 2) + 2 * x + (6 / x);
-
-            return Math.Round(result, 3);
+            return 0.007;
         }
+
+        return (x + 20 > y * 2)
+            ? Math.Pow(x * ((y + 1) / (x + 2)), x)
+            : Math.Pow(y, 2) + 2 * x + (6 / x);
     }
 }
