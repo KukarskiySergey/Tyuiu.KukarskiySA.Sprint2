21368aa5b9a30f9cc9f7b3ac54c338516bd4ca2d
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-09-23 21:15:57 +0500

    Repair Task4

--- Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib/DataService.cs
+++ Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib/DataService.cs
@@ -6,9 +6,8 @@ namespace Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib
     {
         public double Calculate(double x, double y)
         {
-            // Вычисление z с использованием тернарного оператора
             double result = (x + 20 > y * 2)
-                ? Math.Pow(x * ((y + 1) / (x + 2)), x)
+                ? Math.Pow((x + 1) / (x + 2), x)
                 : Math.Pow(y, 2) + 2 * x + (6 / x);
 
             return Math.Round(result, 3); // Округление до 3 знаков после запятой
