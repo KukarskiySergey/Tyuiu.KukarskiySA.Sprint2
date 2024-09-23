cca6c9fd043c90ed21973e44d1def3a0fbcb6909
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-09-23 21:40:59 +0500

    Task4

--- Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib/DataService.cs
+++ Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib/DataService.cs
@@ -1,23 +1,22 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
 
-namespace Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib
+namespace Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib;
+
+public class DataService : ISprint2Task4V6
 {
-    public class DataService : ISprint2Task4V6
+    public double Calculate(double x, double y)
     {
-        public double Calculate(double x, double y)
+        double result;
+
+        if (x + 20 > y * 2)
         {
-            double result;
-
-            if (x + 20 > y * 2)
-            {
-                result = Math.Pow((y + 1) / (x + 2), x);
-            }
-            else
-            {
-                result = Math.Pow(y, 2) + 2 * x + (6 / x);
-            }
-
-            return Math.Round(result, 3);
+            result = Math.Pow((y + 1) / (x + 2), x);
         }
+        else
+        {
+            result = Math.Pow(y, 2) + 2 * x + (6 / x);
+        }
+
+        return Math.Round(result, 3);
     }
 }
