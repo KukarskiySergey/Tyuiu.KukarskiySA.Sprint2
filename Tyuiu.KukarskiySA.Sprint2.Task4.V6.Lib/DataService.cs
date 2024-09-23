009cd95c4a710f4fc3b452cb61975af63737cc45
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-09-23 21:11:44 +0500

    Repair Task4

--- Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib/DataService.cs
+++ Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib/DataService.cs
@@ -2,15 +2,16 @@
 
 namespace Tyuiu.KukarskiySA.Sprint2.Task4.V6.Lib
 {
-    public double Calculate(double x, double y)
+    public class DataService : ISprint2Task4V6
     {
-        if (x == 9 && y == 4)
+        public double Calculate(double x, double y)
         {
-            return 0.007;
-        }
+            // Вычисление z с использованием тернарного оператора
+            double result = (x + 20 > y * 2)
+                ? Math.Pow(x * ((y + 1) / (x + 2)), x)
+                : Math.Pow(y, 2) + 2 * x + (6 / x);
 
-        return (x + 20 > y * 2)
-            ? Math.Pow(x * ((y + 1) / (x + 2)), x)
-            : Math.Pow(y, 2) + 2 * x + (6 / x);
+            return Math.Round(result, 3); // Округление до 3 знаков после запятой
+        }
     }
 }
